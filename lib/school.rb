8f9adf195512cabaffe72c9823335b6545d3a9a6
kelly r maass <kmaass77627@gmail.com>
2019-11-02 23:56:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+class School 
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def name=(name)
+    @name = name 
+  end 
+  
+  def name 
+    @name 
+  end 
+
+def initialize(roster)
+  @roster = {}
+end 
+
+def roster=(roster)
+  @roster = {}
+end 
+  
+def roster 
+  @roster
+end 
+
+def initialize(add_student) 
+  @add_student = add_student
+end 
+  
+
+end 
+
+
+    
